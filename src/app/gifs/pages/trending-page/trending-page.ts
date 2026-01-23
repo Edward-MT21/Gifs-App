f618ad5840795789c9d0ba6a94f3ecca9d247f4c
Edward Malte <eedwardmaltee.270@gmail.com>
2026-01-23 17:04:59 -0500

    Let's think about components

---
+++ src/app/gifs/pages/trending-page/trending-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trending-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './trending-page.html',
+})
+export default class TrendingPage { }
