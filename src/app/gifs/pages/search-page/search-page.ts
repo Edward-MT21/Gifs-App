0c9dd66161995a09dcc4bd0d819e4aba301760b9
Edward Malte <eedwardmaltee.270@gmail.com>
2026-02-04 12:56:49 -0500

    Show result in screen

--- src/app/gifs/pages/search-page/search-page.ts
+++ src/app/gifs/pages/search-page/search-page.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { GifList } from "../../components/gif-list/gif-list";
+import { GifService } from '../../services/gif.service.';
+import { Gif } from '../../interfaces/gif.interface';
 
 @Component({
   selector: 'app-search-page',
   standalone: true,
-  imports: [],
+  imports: [GifList],
   templateUrl: './search-page.html',
 })
-export default class SearchPage { }
+export default class SearchPage {
+
+  gifService = inject(GifService);
+  gifs = signal<Gif[]>([]);
+
+  onSearch(argSearch: string) {
+    console.log({argSearch})
+    this.gifService.searchGifs(argSearch).subscribe(
+      (resp) => this.gifs.set(resp)
+    );
+  }
+
+}
--- src/app/gifs/services/gif.service..ts
+++ src/app/gifs/services/gif.service..ts
@@ -4,6 +4,8 @@ import { environment } from '@environments/environment.development';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { GifMapper } from '../mappers/gif.maper';
 import { Gif } from '../interfaces/gif.interface';
+import { map, tap } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +18,7 @@ export class GifService {
   trendingGifsLoading = signal(true);
 
   constructor() {
-    this.loadTrendingGifs();
+    //this.loadTrendingGifs();
    }
 
   loadTrendingGifs(){
@@ -40,6 +42,27 @@ export class GifService {
         console.log({gifs});
 
       }
+    );
+
+  }
+
+    searchGifs(arggSearch: string){
+
+    return this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`,
+      {
+        params: {
+          api_key: "ko5rmjwedQsKpDhuUHzCbvMdeGNHek1D",
+          limit: 10,
+          q: arggSearch
+        }
+
+      }
+    ).pipe(
+      tap(() => console.log("Entro pipe")),
+      map(({data})=> data),
+      map((items)=> GifMapper.mapGiphyItemsToGifArray(items))
+      //map(({data})=>  GifMapper.mapGiphyItemsToGifArray(data))//Example two in one line
+
     );
 
   }
